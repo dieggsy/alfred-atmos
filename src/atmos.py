dd4c41c9564e3b000025cb55ba736f80211e590e
therockmandolinist <diegoamundo@gmail.com>
2016-07-30 18:48:03 -0700

    Add units

--- src/atmos.py
+++ src/atmos.py
@@ -70,13 +70,13 @@ def main(wf):
             query = re.sub('[^\d.]', '', query, flags=re.I)
             vals = atmosisa(float(query))
 
-        wf.add_item(str(vals[0][0]), icon='T.png',
+        wf.add_item(str(vals[0][0])+' K', icon='T.png',
                     arg=str(vals[0][0]), valid=True)
-        wf.add_item(str(vals[1][0]), icon='a.png',
+        wf.add_item(str(vals[1][0])+' m/s', icon='a.png',
                     arg=str(vals[1][0]), valid=True)
-        wf.add_item(str(vals[2][0]), icon='P.png',
+        wf.add_item(str(vals[2][0])+' N/m^2', icon='P.png',
                     arg=str(vals[2][0]), valid=True)
-        wf.add_item(str(+vals[3][0]), icon='rho.png',
+        wf.add_item(str(+vals[3][0])+' kg/m^3', icon='rho.png',
                     arg=str(vals[3][0]), valid=True)
     except:
         wf.add_item('Incorrect input', icon='icon.png')
